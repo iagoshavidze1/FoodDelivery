ba1cdae09cccf2944e54d14011116583f259fde8
shavidze <sabashavidze@gmail.com>
2021-04-30 21:36:41 +0400

    added-sign-up

--- food-delivery/src/components/auth/sign-up/sign-up.js
+++ food-delivery/src/components/auth/sign-up/sign-up.js
@@ -1,43 +1,87 @@
+import { Formik } from "formik";
+import React, { useState } from "react";
+
 function SignUp() {
-    return (
-        <form style={{ width: "400px" }}>
-            <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter email"
-                ></input>
-                <small id="emailHelp" class="form-text text-muted">
-                    We'll never share your email with anyone else.
-                </small>
-            </div>
-            <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                    placeholder="Password"
-                ></input>
-            </div>
-            <div className="form-group form-check">
-                <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id="exampleCheck1"
-                ></input>
-                <label className="form-check-label" for="exampleCheck1">
-                    Check me out
-                </label>
-            </div>
-            <button type="submit" className="btn btn-primary">
-                Submit
-            </button>
+  debugger;
+  let [name, setName] = useState("");
+
+  return (
+    <Formik
+      initialValues={{ email: "", firstName: "", lastName: "", password: "" }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = "email is required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        if (!values.firstName) errors.firstName = "firstName is required";
+
+        if (!values.lastName) errors.lastName = "lastName is required";
+
+        if (!values.password) errors.password = "password is required";
+
+        return errors;
+      }}
+      onSubmit={(values) => {
+        debugger;
+        console.log(values);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+
+          <input
+            type="text"
+            name="firstName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.firstName}
+          />
+          {errors.firstName && touched.firstName && errors.firstName}
+
+          <input
+            type="text"
+            name="lastName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.lastName}
+          />
+          {errors.lastName && touched.lastName && errors.lastName}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
         </form>
-    );
+      )}
+    </Formik>
+  );
 }
 
 export default SignUp;
--- food-delivery/src/routes.js
+++ food-delivery/src/routes.js
@@ -1,21 +1,22 @@
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import SignUp from "components/auth/sign-up/Sign-Up";
+
 import SignIn from "components/auth/sign-in/Sign-In";
 import MainLayout from "hocs/mainLayout";
 
 const Routes = (props) => {
-    return (
-        <BrowserRouter>
-            <MainLayout>
-                <Switch>
-                    <Route path="/sign-in" component={SignIn} />
-                    <Route path="/sign-up" component={SignUp} />
-                    <Route path="/" component={SignIn} />
-                </Switch>
-            </MainLayout>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <MainLayout>
+        <Switch>
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/" component={SignIn} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
